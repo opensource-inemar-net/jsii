938221bcc76188f3c4f80d67351afa61c326843d
Oliver Schnabl <opensource@inemar.net>
2020-05-28 13:25:07 +0000

    Added files to build code

---
+++ js.sh
@@ -0,0 +1,7 @@
+sudo npm install  ./dist/js/@jsii-spec.tgz
+sudo npm install  ./dist/js/jsii-rosetta.tgz
+sudo npm install  ./dist/js/oo-ascii-tree.tgz 
+sudo npm install  ./dist/js/jsii-reflect.tgz  
+sudo npm install  ./dist/js/codemaker-0.0.0.tgz
+sudo npm install  ./dist/js/jsii-pacmak.tgz 
+sudo npm install  ./dist/js/jsii.tgz 
